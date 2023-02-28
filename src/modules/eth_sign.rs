347492f7ea35baad45a039376fd68601e2bc9481
mananabot <manabot@gmail.com>
2023-02-28 12:57:37 +0100

    Remove dbg

--- src/modules/eth_sign.rs
+++ src/modules/eth_sign.rs
@@ -45,7 +45,6 @@ pub fn sign_onboarding(
     private_key: &str,
 ) -> PyResult<String> {
     let path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/eth_signing"));
-    dbg!("path: {}", path);
     let py_app = fs::read_to_string(path.join("eth_sign.py"))?;
 
     let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
@@ -56,7 +55,6 @@ pub fn sign_onboarding(
             .into();
         app.call1(py, (network_id, ethereum_address, action, private_key))
     });
-    
     Ok(from_python.unwrap().to_string())
 }
 
